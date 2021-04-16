463fd25fa2b0fbc7289802069d919c2eb1ec991b
benweizhu <benweizhu@gmail.com>
2021-04-16 12:17:13 +0800

    Add Order and OrderItem

---
+++ Ordering.Domain/Order/Order.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Ordering.Domain.Order
+{
+    public class Order
+    {
+        private long Id { get; }
+        private List<OrderItem> OrderItems { get; }
+
+        public Order(int id, List<OrderItem> orderItems)
+        {
+            Id = id;
+            OrderItems = orderItems;
+        }
+
+        public void AddOrderItem(OrderItem orderItem)
+        {
+            this.OrderItems.Add(orderItem);
+        }
+    }
+}
---
+++ Ordering.Domain/Order/OrderItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Ordering.Domain.Order
+{
+    public class OrderItem
+    {
+        private long Id { get; }
+        private string Name { get; }
+
+        public OrderItem(long id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
